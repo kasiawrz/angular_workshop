e22d3b3bb300a38735093e9cf457bdc65bad0f44
Kasia_Wrzesniak <wrzesniak.k@gmail.com>
2017-07-22 15:32:35 +0200

    Run the movies search engine

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,16 +9,28 @@ import { Show, ShowsResponse} from "../tv.models";
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  //http;
   shows: Show[];
+  query = 'flash';
 
 
 
-  constructor(http: Http) {
-    const apiUrl = 'http://api.tvmaze.com/search/shows?q=flash';
-    http.get(apiUrl)
+  constructor(private http: Http) {
+   //? this.search(this.query);
+
+
+    // const apiUrl = 'http://api.tvmaze.com/search/shows?q=flash';
+    // http.get(apiUrl)
+    //   .map(resp => resp.json() as ShowsResponse[])
+    //   .subscribe(shows => this.shows = shows.map(showMeta => showMeta.show));  //bez subskrybcji to sama definicja, sub zaczyna obserwować; wykonuje fn
+  }
+
+  search(query: String) {
+    const apiUrl = `http://api.tvmaze.com/search/shows?q=${query}`;
+    this.http.get(apiUrl)  //??? this? bez przekazania jako argumnebt
       .map(resp => resp.json() as ShowsResponse[])
       .subscribe(shows => this.shows = shows.map(showMeta => showMeta.show));  //bez subskrybcji to sama definicja, sub zaczyna obserwować; wykonuje fn
+
   }
 
   ngOnInit() {
